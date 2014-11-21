c4267719bffbb05b0a6f5c0073461cfa36fcd9ca
cuipengfei <cuipengfei2008@gmail.com>
2014-11-21 18:53:11 +0800

    替换实例

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,4 +1,9 @@
 require './item.rb'
+require './normal_item.rb'
+require './immutable_item.rb'
+require './aged_item.rb'
+require './fast_item.rb'
+require './time_limit_item.rb'
 
 class GildedRose
   attr_accessor :items
@@ -7,12 +12,12 @@ class GildedRose
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    @items << NormalItem.new("+5 Dexterity Vest", 10, 20)
+    @items << AgedItem.new("Aged Brie", 2, 0)
+    @items << NormalItem.new("Elixir of the Mongoose", 5, 7)
+    @items << ImmutableItem.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << TimeLimitItem.new("Backstage passes to a TAFKAL80ETC concert", 15, 20, {10 => 2, 5 => 3})
+    @items << FastItem.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
