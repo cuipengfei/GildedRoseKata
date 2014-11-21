fd1966635eeb7c361e3e5391c5509acfdf773557
cuipengfei <cuipengfei2008@gmail.com>
2014-11-21 18:50:37 +0800

    补全测试

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -19,4 +19,41 @@ describe GildedRose do
     expect(subject.items[0].quality).to eq(8)
   end
 
+  it "normal item should never have quality less than zero" do
+    30.times { |num| subject.update_quality }
+    expect(subject.items[0].quality).to eq(0)
+  end
+
+  it "immutable item should not change at all" do
+    1000.times { |num| subject.update_quality }
+    expect(subject.items[3].quality).to eq(80)
+  end
+
+  it "aged item should increase" do
+    subject.update_quality
+    expect(subject.items[1].quality).to eq(1)
+
+    subject.update_quality
+    expect(subject.items[1].quality).to eq(2)
+  end
+
+  it "aged item should increase more than 50" do
+    1000.times { |num| subject.update_quality }
+    expect(subject.items[1].quality).to eq(50)
+  end
+
+  it "time limit item should increase according to time slots" do
+    5.times { |num| subject.update_quality }
+    expect(subject.items[4].quality).to eq(25)
+
+    5.times { |num| subject.update_quality }
+    expect(subject.items[4].quality).to eq(35)
+
+    5.times { |num| subject.update_quality }
+    expect(subject.items[4].quality).to eq(50)
+
+    1.times { |num| subject.update_quality }
+    expect(subject.items[4].quality).to eq(0)
+  end
+
 end
